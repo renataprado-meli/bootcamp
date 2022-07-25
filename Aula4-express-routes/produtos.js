d44ce0fef8cf0c8c4ab9eabb3d413830f429cd1b
Renata Prado <renata.prado@mercadolivre.com>
2022-07-25 16:37:32 -0300

    auth

---
+++ Aula4-express-routes/produtos.js
@@ -0,0 +1,10 @@
+let produtos = [
+    { name: "Macbook", id: 1, price: 1300, quantity: 40, colors: ["silver", "black", "white"] },
+    { name: "Iphone", id: 2, price: 1000, quantity: 50, colors: ["silver", "red", "white"] },
+    { name: "Pendrive", id: 3, price: 10, quantity: 10, colors: [] },
+    { name: "Headset", id: 4, price: 50, quantity: 0, colors: ["black"] },
+    { name: "Mouse", id: 5, price: 20, quantity: 5, colors: ["white", "black", "blue"] },
+    { name: "Tablet", id: 6, price: 500, quantity: 20, colors: ["white", "black"] }
+];
+
+module.exports = produtos;
